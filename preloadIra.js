ccf67d4a0b5c11e0bbce94dd149d7b7971bcf62a
Ремизова Ирина <w.warmbl@gmail.com>
2022-10-08 21:36:37 +0500

    create Ira

---
+++ preloadIra.js
@@ -0,0 +1,19 @@
+// Юля
+const pre_model_massive = [
+    './model/ira/ira2.png',
+];
+const pre_rita_hair_massive = [
+    './model/ira/hair/1.png',
+    './model/ira/hair/2.png',
+    './model/ira/hair/3.png',
+    './model/ira/hair/4.png',
+    './model/ira/hair/5.png',
+];
+const pre_rita_clother_massive = [
+    './model/ira/clother/1.png',
+    './model/ira/clother/2.png',
+    './model/ira/clother/3.png',
+];
+const pre_rita_decorations_massive = [
+    './model/ira/decorations/1.png',
+];
